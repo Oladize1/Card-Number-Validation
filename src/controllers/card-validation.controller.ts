b7c82bcbf1d51facde6fe20fcef5f6cb3d80ba85
marcus <pamoladize10@gmail.com>
2026-09-02 11:29:27 +0100

    feat: add POST /validate route and controller skeleton

---
+++ src/controllers/card-validation.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express"
+export const cardValidationController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
